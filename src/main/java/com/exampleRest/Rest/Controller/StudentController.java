78d147601975c3b83ee49e92ddf9b9a6644dbbfc
yigit <myigitsyn@gmail.com>
2024-09-08 21:31:26 +0300

    REST APIs convert to ResponseEntity

--- src/main/java/com/exampleRest/Rest/Controller/StudentController.java
+++ src/main/java/com/exampleRest/Rest/Controller/StudentController.java
@@ -3,6 +3,7 @@ package com.exampleRest.Rest.Controller;
 import com.exampleRest.Rest.Exception.StudentIDZeroException;
 import com.exampleRest.Rest.bean.Student;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -113,6 +114,84 @@ public class StudentController {
         return "Student deleted successfully";
 
     }
+
+    //
+    @GetMapping("student/getStudentResponseEntity")
+    public ResponseEntity<Student> getStudentResponseEntity(){
+        // obje döndüğümüzde format json oluyor.
+        Student student = new Student(1,"Yigit","Sayin");
+        //return new ResponseEntity<>(student,HttpStatus.OK);
+        //return ResponseEntity.ok(student);
+        //postman header
+        return ResponseEntity.ok().header("custom-header","yigit").body(student);
+    }
+
+    //http://localhost:8080/students/getStudentsResponseEntity
+    @GetMapping("/students/getStudentsResponseEntity")
+    public ResponseEntity<List<Student>> getStudentsResponseEntity(){
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student(1,"Yigit","Sayin"));
+        students.add(new Student(2,"Yigit","Sayin"));
+        students.add(new Student(3,"Yigit","Sayin"));
+        students.add(new Student(4,"Yigit","Sayin"));
+        return ResponseEntity.ok(students);
+    }
+
+
+    //http://localhost:8080/students/1/yig/say
+    @GetMapping("students/studentPathVariableAllVariableResponseEntity/{id}/{firstName}/{lastName}")
+    public ResponseEntity<Student> studentPathVariableAllVariableResponseEntity(@PathVariable("id") int studentId,
+                                                  @PathVariable String firstName,
+                                                  @PathVariable String lastName) {
+        if (studentId == 0) {
+            throw new StudentIDZeroException("Input cannot be zero.");
+        } else {
+            Student student = new Student(studentId, firstName, lastName);
+            return ResponseEntity.ok(student);
+
+        }
+
+
+
+    }
+    //http://localhost:8080/students/query/studentRequestVariableAll?id=1&firstName=yig&lastName=yig
+    @GetMapping("students/query/studentRequestVariableAllResponseEntity")
+        public ResponseEntity<Student> studentRequestVariableAllResponseEntity(@RequestParam int id, @RequestParam("firstName") String first,@RequestParam("lastName") String last){
+             Student student = new Student(id,first,last);
+             return ResponseEntity.ok(student);
+        }
+
+    //We don't need to use @HttpStatus annotation ResponseEntity handle in the return
+    @PostMapping("students/createStudentResponseEntity")
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Student> createStudentResponseEntity(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @PutMapping("students/{id}/updateStudentResponseEntity")
+    public ResponseEntity<Student> updateStudentResponseEntity(@RequestBody Student student,@PathVariable("id") int studentId ){
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return ResponseEntity.ok(student);
+    }
+
+    @DeleteMapping("students/{id}/deleteStudentResponseEntity")
+    //@ResponseStatus(HttpStatus.NO_CONTENT)
+    // HttpStatus.NO_CONTENT (204): Sunucu isteği başarılı bir şekilde işlediğini belirtir ancak yanıt gövdesi boştur. Gövdeye yazılan bir veri olsa bile, kullanıcıya gönderilmez.
+    //HttpStatus.CREATED (201): Sunucu isteği başarılı bir şekilde işlediğini ve yeni bir kaynak oluşturduğunu belirtir. Yanıt gövdesinde "selamlar" gibi bir içerik gönderilebilir.
+    public ResponseEntity<String> deleteStudentResponseEntity(@PathVariable("id") int studentId){
+        System.out.println(studentId);
+        //return new ResponseEntity<>("selamlar", HttpStatus.NO_CONTENT);
+        //return new ResponseEntity<>("selamlar", HttpStatus.CREATED);
+        return ResponseEntity.ok("deleted successfully");
+
+    }
+
+
+
 
 }
 

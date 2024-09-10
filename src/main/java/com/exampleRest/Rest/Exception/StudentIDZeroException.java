f4d63c54fbf8373d9c36856f595327c9be8fc62b
yigit <myigitsyn@gmail.com>
2024-09-10 14:00:01 +0300

    vs

--- src/main/java/com/exampleRest/Rest/Exception/StudentIDZeroException.java
+++ src/main/java/com/exampleRest/Rest/Exception/StudentIDZeroException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_GATEWAY)
+@ResponseStatus(HttpStatus.NO_CONTENT)
 public class StudentIDZeroException extends RuntimeException{
     public StudentIDZeroException(String message) {
         super(message);
